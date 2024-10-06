f6f97b383d34caa8c79e9dd51279053cdef83cc2
samsonssali <ssali@jitpomi.com>
2024-10-05 18:42:06 -0600

    add tailwind css and qwikui

--- packages/time-travel-demo-qwik/src/routes/index.tsx
+++ packages/time-travel-demo-qwik/src/routes/index.tsx
@@ -1,19 +1,22 @@
 import {component$, useSignal, useTask$} from '@builder.io/qwik';
 import type { DocumentHead } from "@builder.io/qwik-city";
 import { sayHi } from "@loro-explore/shared";
+import { Separator } from '@qwik-ui/headless';
 export default component$(() => {
-  const greeting = useSignal<string>()
+  const greeting = useSignal<string>('')
   useTask$(() => {
     greeting.value = sayHi('Loro!');
   });
   return (
     <>
-      <h1>{greeting}</h1>
+      <h1 class={'text-red-700'}>{greeting}</h1>
+      <Separator orientation="horizontal" class="separator-top h-1 bg-red-700 w-full" />
       <div>
         Can't wait to see what you build with qwik!
         <br />
         Happy coding.
       </div>
+      
     </>
   );
 });
